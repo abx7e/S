8104d3964f19848b4ba134c5afd9d30e04165d49
abx7e <zwm11995@gmail.com>
2024-05-25 10:16:49 +0300

    Update __init__.py

--- BlThon/assistant/__init__.py
+++ BlThon/assistant/__init__.py
@@ -1,4 +1,4 @@
-from BlackThon import BOTLOG, BOTLOG_CHATID, BlThon
+from BlackThon import BOTLOG, BOTLOG_CHATID, 
 from BlThon.razan.resources.assistant import *
 from ..Config import Config
 from ..core.inlinebot import *
