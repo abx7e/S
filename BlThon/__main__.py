f3ae3e59984e2544b135134184d24166ad8d8b1d
abx7e <zwm11995@gmail.com>
2024-05-25 10:45:59 +0300

    Update __main__.py

--- BlThon/__main__.py
+++ BlThon/__main__.py
@@ -1,6 +1,5 @@
 import sys
 import contextlib
-import BlackThonUB
 from BlackThonUB import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
 from .Config import Config
 from .core.logger import logging
