50dd0106cb5ac326799f213bd03853c4040bc44c
abx7e <zwm11995@gmail.com>
2024-05-25 10:17:38 +0300

    Update bot_pms.py

--- BlThon/assistant/bot_pms.py
+++ BlThon/assistant/bot_pms.py
@@ -8,7 +8,7 @@ from telethon.errors import UserIsBlockedError
 from telethon.events import CallbackQuery, StopPropagation
 from telethon.utils import get_display_name
 
-from BlackThon import Config, BlThon
+from BlackThon import Config
 
 from ..core import check_owner, pool
 from ..core.logger import logging
