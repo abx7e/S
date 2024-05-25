10fb1a1691dd7a03b525e302d69b1f3333e0ec03
abx7e <zwm11995@gmail.com>
2024-05-25 11:29:11 +0300

    Update اوامر الادمن الوقتية.py

--- BlThon/plugins/اوامر الادمن الوقتية.py
+++ BlThon/plugins/اوامر الادمن الوقتية.py
@@ -3,7 +3,7 @@ from telethon.errors.rpcerrorlist import UserAdminInvalidError, UserIdInvalidErr
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights
 
-from BlackThonUB import BlThon
+from BlackThonUB import *
 
 from ..core.managers import edit_or_reply
 from ..helpers.utils import _format
